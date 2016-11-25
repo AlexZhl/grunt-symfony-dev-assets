fe8452c1518ff57bd4440016a31aff5b9165896b
Stadnik, Andrey <a.stadnik@itransition.com>
2016-11-25 16:16:43 +0300

    first

---
+++ tasks/symfony_dev_assets.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-symfony-dev-assets
+ * https://github.com/enemis/grunt-symfony-dev-assets
+ *
+ * Copyright (c) 2016 Stadnik, Andrey
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('symfony_dev_assets', 'Create twig two templates. The first one contain all js assets and second one css assets', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
